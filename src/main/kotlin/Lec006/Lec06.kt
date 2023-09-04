732ee7f374b1b1458094d5f6c82033925cb27a76
윤원용 <2dawit12@naver.com>
2023-09-04 23:48:40 +0900

    6o

--- src/main/kotlin/Lec006/Lec06.kt
+++ src/main/kotlin/Lec006/Lec06.kt
@@ -1,4 +1,21 @@
 package Lec006
 
-class Lec06 {
+fun main(){
+    val numbers = listOf(1L, 2L, 3L)
+    for(number in numbers){
+        println(number)
+    }
+}
+
+fun main1(){
+    for(i in 1..3){
+        println(i)
+    }
+    for(e in 3 downTo 1){
+        println(e)
+    }
+    for(z in 1..5 step 2){
+        println(z)
+    }
+
 }
